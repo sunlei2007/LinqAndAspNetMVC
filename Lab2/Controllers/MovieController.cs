e6f3ca14ab9183b166292d06b7e2bd4a979a74de
Lei Sun <139188566@qq.com>
2023-02-10 08:17:16 -0600

    M

---
+++ Lab2/Controllers/MovieController.cs
@@ -0,0 +1,50 @@
+﻿using Lab2.Data;
+using Lab2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetAllMovies()
+        {
+
+            return View(Context.movies.ToList<Movie>());
+        }
+        public IActionResult MovieDetails(int id)
+        {
+            ViewBag.MovieId = id;
+            List<Role> roles=Context.roles.Where(c=>c.Movie.MovieId==id).ToList<Role>(); 
+            return View(roles);
+        }
+        public IActionResult CreateCommentView(int movieId,int type)
+        {
+            ViewBag.MovieId = movieId;
+            ViewBag.Type = type;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateCommentView(Rating rating,int id,int type)
+        {
+            if (rating.User.Name == rating.UserName)
+            {
+                return NotFound();
+            }
+             rating.RatingId = Context.ratings.Count + 1;
+             rating.RatedId = id;
+             rating.Type = type;
+             rating.User = Context.users.FirstOrDefault<User>(c=>c.Name==rating.UserName);
+             Context.ratings.Add(rating);
+
+             
+             return RedirectToAction("MovieDetails", "Movie", new { id = id});
+        }
+        
+    }
+}
---
+++ Lab2/Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿namespace Lab2.Models
+{
+    public class Movie
+    {
+        private int _movieId;
+        public int MovieId { get { return _movieId; } }
+
+        private string _title;
+        public string Title { get { return _title; } }
+
+        private string _genre;
+        public string Genre { get { return _genre; } }
+
+
+        private int _budget;
+        public int Budget { get { return _budget; } }
+
+        private int _years;
+        public int Years { get { return _years; } }
+
+       
+
+        public Movie(int movieId, string title,string genre,int budget,int years)
+        {
+            _movieId = movieId;
+            _title = title;
+            _genre = genre;
+            _budget = budget;
+            _years = years;
+        }
+    }
+}
